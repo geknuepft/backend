a55721b75239dac41172f199e61778be8d913f4f
Lorenz Koestler (lko) <lorenz.koestler@datahouse.ch>
2016-11-25 21:30:03 +0100

    refactor GET v0/articles

--- article.go
+++ article.go
@@ -2,18 +2,83 @@ package main
 
 import (
 	"gopkg.in/guregu/null.v3"
+	"log"
+	"strings"
 )
 
 type Article struct {
-	Id           int         `json:"article_id"`
-	Name         string      `json:"name"`
+	Id           int         `json:"article_id"    db:"article_id"`
+	Name         string      `json:"name"          db:"article_name"`
 	Pictures     PictureMap  `json:"pictures"`
-	InstanceId   null.Int    `json:"instance_id"`
-	LengthMm     null.Int    `json:"length_mm"`
-	WidthMm      null.Int    `json:"width_mm"`
-	HeightMm     null.Int    `json:"height_mm"`
-	PriceCchf    null.Int    `json:"price_cchf"`
-	CollectionDe null.String `json:"collection_de"`
+	InstanceId   null.Int    `json:"instance_id"   db:"instance_id"`
+	LengthMm     null.Int    `json:"length_mm"     db:"length_mm""`
+	WidthMm      null.Int    `json:"width_mm"      db:"width_mm"`
+	HeightMm     null.Int    `json:"height_mm"     db:"height_mm"`
+	PriceCchf    null.Int    `json:"price_cchf"    db:"price_cchf"`
+	CollectionDe null.String `json:"collection_de" db:"collection_de"`
+}
+
+type ArticleRow struct {
+	Article
+	Path0 null.String `db:"path0"`
 }
 
 type Articles []Article
+
+func GetArticles() (articles Articles) {
+
+	var db = getDbX()
+	defer db.Close()
+
+	var picturePrefixes = [...]string{"cma0"}
+
+	var qs = `SELECT
+        -- article fields
+        a.article_id,
+        COALESCE(a.article_name_de, c.category_de) article_name,
+        i0.path path0,
+        -- instance fields
+        i.instance_id,
+        i.length_mm,
+        i.width_mm,
+        i.height_mm,
+        i.price_cchf,
+        ic.collection_de
+        FROM article a
+        JOIN category c ON(c.category_id = a.category_id)
+        JOIN image_type it0 ON(it0.abbr = '` + picturePrefixes[0] + `')
+        JOIN image i0 ON(i0.article_id = a.article_id AND i0.image_type_id = it0.image_type_id)
+        LEFT JOIN instance i ON(i.article_id = a.article_id)
+        LEFT JOIN collection ic ON(ic.collection_id = i.collection_id)
+        GROUP BY a.article_id -- in case an article has >1 cma0
+        ORDER BY a.created DESC`
+
+	rows, err := db.Queryx(qs)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	var articleRow = ArticleRow{}
+
+	for rows.Next() {
+		err := rows.StructScan(&articleRow)
+		if err != nil {
+			log.Printf(err.Error())
+			break
+		}
+
+		a := &articleRow.Article
+
+		if articleRow.Path0.Valid {
+			a.Pictures = make(PictureMap)
+			a.Pictures[picturePrefixes[0]] = Picture{
+				Path: strings.Trim(articleRow.Path0.String, "\n\r "),
+				Type: picturePrefixes[0],
+			}
+		}
+
+		articles = append(articles, *a)
+	}
+
+	return
+}
--- db.go
+++ db.go
@@ -3,8 +3,6 @@ package main
 import (
 	"database/sql"
 	"github.com/jmoiron/sqlx"
-	"log"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -31,72 +29,6 @@ func getDbX() (db *sqlx.DB) {
 	db, err = sqlx.Connect("mysql", "geknuepft:Er3cof4iesho@tcp(mysql-server:3306)/geknuepft")
 	if err != nil {
 		panic(err.Error())
-	}
-
-	return
-}
-
-func GetArticles() (articles Articles) {
-	var Db *sql.DB
-	Db = getDb()
-	defer Db.Close()
-
-	var qs string
-
-	qs = `SELECT
-        -- article fields
-        a.article_id,
-        COALESCE(a.article_name_de, c.category_de) article_name,
-        i0.path p0,
-        -- instance fields
-        i.instance_id,
-        i.length_mm,
-        i.width_mm,
-        i.height_mm,
-        i.price_cchf,
-        ic.collection_de
-        FROM article a
-        JOIN category c ON(c.category_id = a.category_id)
-        JOIN image_type it0 ON(it0.abbr = 'cma0')
-        JOIN image i0 ON(i0.article_id = a.article_id AND i0.image_type_id = it0.image_type_id)
-        LEFT JOIN instance i ON(i.article_id = a.article_id)
-        LEFT JOIN collection ic ON(ic.collection_id = i.collection_id)
-        GROUP BY a.article_id -- in case an article has >1 cma0
-        ORDER BY a.created DESC`
-
-	log.Printf("%s", qs)
-
-	rows, err := Db.Query(qs)
-
-	if err != nil {
-		panic(err.Error())
-	}
-	defer rows.Close()
-
-	var picturePrefixes = [...]string{"cma0"}
-
-	for rows.Next() {
-		var a Article
-
-		var pictures [1]sql.NullString
-
-		err := rows.Scan(&a.Id, &a.Name, &pictures[0], &a.InstanceId, &a.LengthMm,
-			&a.WidthMm, &a.HeightMm, &a.PriceCchf, &a.CollectionDe)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		a.Pictures = make(PictureMap)
-
-		for i, p := range pictures {
-			if p.Valid {
-				a.Pictures[picturePrefixes[i]] = Picture{
-					Path: strings.Trim(p.String, "\n\r "),
-					Type: picturePrefixes[i],
-				}
-			}
-		}
-		articles = append(articles, a)
 	}
 
 	return
